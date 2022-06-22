87d48c16f1dbf581aad9c24aa38c8ad6b05b0924
Laura Miller <lam9@unc.edu>
2022-06-22 15:02:52 -0700

    fixed ocelot script

--- AZ-Examples/2-IBAMR-Example_2Drubber_band/ocelot_script.sh
+++ AZ-Examples/2-IBAMR-Example_2Drubber_band/ocelot_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=IBAMR-test
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=04:00:00
-#SBATCH --partition=windfall
+#SBATCH --partition=standard
+#SBATCH --account=lauram9
 #SBATCG --output IBAMR2D.out
 
 mpirun ./main2d input2d
